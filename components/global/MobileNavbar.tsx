ab106f624fe2744bb8113378269d66154d7ac949
Umang Chauhan <umang350@gmail.com>
2022-08-21 15:05:33 +0900

    mobile

--- components/global/MobileNavbar.tsx
+++ components/global/MobileNavbar.tsx
@@ -1,12 +1,126 @@
-import { useEffect, useState } from "react";
+import { SVGProps, useEffect, useState } from "react";
 
 import Link from "next/link";
 import {routes} from "../../data/global";
+import useDelayedRender from "use-delayed-render";
+import styles from './MobileNavbar.module.css';
 
 export default function MobileNavbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { mounted: isMenuMounted, rendered: isMenuRendered } = useDelayedRender(
+    isMenuOpen,
+    {
+      enterDelay: 20,
+      exitDelay: 300,
+    }
+  );
 
+  function toggleMenu() {
+    if (isMenuOpen) {
+      setIsMenuOpen(false);
+      document.body.style.overflow = "";
+    } else {
+      setIsMenuOpen(true);
+      document.body.style.overflow = "hidden";
+    }
+  }
+
+  useEffect(() => {
+    return function cleanup() {
+      document.body.style.overflow = "";
+    };
+  }, []);
   return (
     <nav>
+      <div
+        className={`w-full justify-between flex items-center ${isMenuRendered && 'bg-bg'} p-5`}
+        style={{ zIndex: 101 }}
+      >
+        <li className="list-none font-bold text-lg">
+          <Link href="/">
+            <h1>umang.dev</h1>
+          </Link>
+        </li>
+        <button
+          className={`${styles.burger} visible md:hidden`}
+          aria-label="Toggle menu"
+          type="button"
+          onClick={toggleMenu}
+        >
+          <MenuIcon data-hide={isMenuOpen} />
+          <CrossIcon data-hide={!isMenuOpen} />
+        </button>
+      </div>
+      {isMenuMounted && (
+        <ul
+          className={`${styles.menu} flex flex-col absolute bg-bg
+            ${isMenuRendered && styles.menuRendered}`}
+        >
+          {routes.map((item, index) => {
+            return (
+              <li
+                className="border-b border-gray-900 text-gray-100 text-sm font-semibold"
+                style={{ transitionDelay: `${150 + index * 25}ms` }}
+                key={item.title}
+              >
+                <Link href={item.path}>
+                  <a className="flex w-auto pb-4">{item.title}</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </nav>
+  );
+}
+
+
+function MenuIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      className="h-5 w-5 absolute text-gray-100"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      {...props}
+    >
+      <path
+        d="M2.5 7.5H17.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2.5 12.5H17.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+function CrossIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      className="h-5 w-5 absolute text-gray-100"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="none"
+      shapeRendering="geometricPrecision"
+      {...props}
+    >
+      <path d="M18 6L6 18" />
+      <path d="M6 6l12 12" />
+    </svg>
   );
 }
