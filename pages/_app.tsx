ebce059c52956a0e3247059a869a9aa282a405f8
Umang Chauhan <umang350@gmail.com>
2022-08-21 16:04:10 +0900

    gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import "tailwindcss/tailwind.css";
+import { useEffect } from 'react'
+
+import * as ga from '../lib/ga'
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import * as ga from '../../lib/ga'
+
 type Data = {
   name: string
 }
@@ -9,5 +11,15 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  
+  const search = () => {
+    ga.event({
+      action: "api",
+      params : {
+        search_term: req
+      }
+    })
+  }
+
   res.status(200).json({ name: 'John Doe' })
 }
