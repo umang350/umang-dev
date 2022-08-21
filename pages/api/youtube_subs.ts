0c3154ef568de649c840991b023335fe1a8ab4fb
Umang Chauhan <umang350@gmail.com>
2022-08-21 22:24:22 +0900

    yt

--- pages/api/youtube_subs.ts
+++ pages/api/youtube_subs.ts
@@ -1,4 +1,5 @@
 import { google } from 'googleapis';
+import "https://apis.google.com/js/api.js";
 
 const youtube = async (_, res) => {
   const auth = new google.auth.GoogleAuth({
@@ -14,9 +15,12 @@ const youtube = async (_, res) => {
     version: 'v3'
   });
 
+  authenticate();
+  const data = execute();
+
   const response = await youtube.channels.list({id: ['UCGuJC8U8zMOpvKdYPsh1M2Q']},{})
 
-  const channel = response.data.items[0];
+  const channel = response.data.items[0] || data.items;
   const { subscriberCount, viewCount } = channel.statistics;
 
   return res.status(200).json({
@@ -24,5 +28,34 @@ const youtube = async (_, res) => {
     viewCount
   });
 };
+
+function authenticate() {
+    return google.auth2.getAuthInstance()
+        .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
+        .then(function() { console.log("Sign-in successful"); },
+              function(err) { console.error("Error signing in", err); });
+  }
+  function loadClient() {
+    google.client.setApiKey(process.env.GOOGLE_PRIVATE_KEY);
+    return google.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  function execute() {
+    return google.client.youtube.channels.list({
+      "part": [
+        "snippet,contentDetails,statistics"
+      ],
+      "id": [
+        "UCGuJC8U8zMOpvKdYPsh1M2Q"
+      ]
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+  }
 
 export default youtube;
