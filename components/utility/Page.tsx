49e704c3524903ff8e0d46b9b0918536924f7633
Umang Chauhan <umang350@gmail.com>
2022-08-21 15:28:23 +0900

    favicon

--- components/utility/Page.tsx
+++ components/utility/Page.tsx
@@ -37,6 +37,14 @@ function Page({ currentPage, meta: { title, desc }, children }: PageProps) {
           href="/favicon/favicon-16x16.png"
         />
         <link rel="manifest" href="/favicon/site.webmanifest" />
+        
+        <link rel="mask-icon" href="/public/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/public/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content="/public/favicon/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+
+
         <meta name="title" content={pageTitle} />
         <meta name="description" content={desc} />
 
