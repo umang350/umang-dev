222ddbeed1dc908df191611a34d3a5d95d98073c
Umang Chauhan <umang350@gmail.com>
2022-08-27 20:34:15 +0900

    About Me

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,12 +1,54 @@
 import Image from "next/image";
+import { Experience } from "./about/Experience";
 
 export const AboutMe = () => {
-    return (
-        <section>
-        <div className="max-w-6xl mx-auto h-48">
-          <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left dark:text-white text-black">
-            About Me.
-          </h1>
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto h-48">
+        <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left dark:text-white text-black">
+          About Me.
+        </h1>
+      </div>
+      <div className="flex">
+        <div className="pt-20 flex-1 max-w-6xl mx-auto gap-y-20 gap-x-20 text-left">
+          <div className="inline-flex flex-col">
+            <div>
+              <h1 className="text-xl font-semibold text-gray-700 dark:text-gray-200">
+                Contact
+              </h1>
+              <p className="text-lg text-gray-500 mt-4 dark:text-gray-300">
+                For any sort help / enquiry, shoot a{" "}
+                <a
+                  href={`mailto:job@umang.dev`}
+                  className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300"
+                >
+                  mail
+                </a>{" "}
+                and I&apos;ll get back. I swear.
+              </p>
+            </div>
+            <div className="mt-8">
+              <h1 className="text-xl font-semibold text-gray-700 dark:text-gray-200">
+                Job Opportunities
+              </h1>
+              <p className="text-lg text-gray-500 mt-4 dark:text-gray-300">
+                I&apos;m looking for a challenge currently, If you see me as a good fit,
+                check my{" "}
+                <a
+                  href="/UmangResume.pdf"
+                  target="__blank"
+                  className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300"
+                >
+                  CV
+                </a>{" "}
+                and I&apos;d love to work with you.
+              </p>
+            </div>
+            </div>
         </div>
-      </section>)
-} 
+        <div className="m-10 p-10 flex-3">
+          <Experience/>
+        </div>
+      </div>
+    </section>)
+}
---
+++ components/about/Experience.tsx
@@ -0,0 +1,57 @@
+export const Experience = () => {
+    return (
+        <>
+            <ExperienceCard
+                key="2"
+                title="Working"
+                desc="Software Developer - IT Planning"
+                year="2020-Now"
+                company="Fast Retailing (UNIQLO)"
+                companyLink="https://www.fastretailing.com/eng/"
+            />
+            <div className="divider-container flex flex-col items-center -mt-2">
+                <div className="w-4 h-4 bg-green-500 rounded-full relative z-10">
+                    <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 animate-ping"></div>
+                </div>
+                <div className="w-1 h-24 bg-gray-200 dark:bg-gray-500 rounded-full -mt-2"></div>
+            </div>
+            <ExperienceCard
+                key="1"
+                title="Internship"
+                desc="Global Internship Program – Technology Intern"
+                year="2019"
+                company="Tata Communications Ltd."
+                companyLink="https://tatacommunications.com"
+            />
+            <div className="divider-container flex flex-col items-center -mt-2">
+                <div className="w-4 h-4 bg-green-500 rounded-full relative z-10">
+                    <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 animate-ping"></div>
+                </div>
+                <div className="w-1 h-24 bg-gray-200 dark:bg-gray-500 rounded-full -mt-2"></div>
+            </div>
+            <ExperienceCard
+                key="0"
+                title="University"
+                desc="Bachelor of Technology (CS &amp; Math)"
+                year="2016-20"
+                company="Delhi Technological University (formerly D.C.E.)"
+                companyLink="https://dtu.ac.in"
+            />
+        </>
+    )
+}
+
+const ExperienceCard = ({ title, desc, year, company, companyLink }) => {
+    return (
+        <div className="relative experience-card border p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4">
+            <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-200 font-bold dark:text-gray-800">
+                {year}
+            </h1>
+            <h1 className="font-semibold text-xl">{title}</h1>
+            <a href={companyLink} className="text-gray-500">
+                {company}
+            </a>
+            <p className="text-gray-600 dark:text-gray-400 my-2">{desc}</p>
+        </div>
+    );
+};
