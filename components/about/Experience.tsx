e1253d9facf6dc5dac8db411df6e562803e4f0c4
Umang Chauhan <umang350@gmail.com>
2022-08-27 20:35:23 +0900

    Update Experience.tsx

--- components/about/Experience.tsx
+++ components/about/Experience.tsx
@@ -44,7 +44,7 @@ export const Experience = () => {
 const ExperienceCard = ({ title, desc, year, company, companyLink }) => {
     return (
         <div className="relative experience-card border p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4">
-            <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-200 font-bold dark:text-gray-800">
+            <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-400 font-bold dark:text-gray-800">
                 {year}
             </h1>
             <h1 className="font-semibold text-xl">{title}</h1>
