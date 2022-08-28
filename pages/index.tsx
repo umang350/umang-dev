60f27faa0bb890070b2509ebe58dd83c1f32e8fc
Umang Chauhan <umang350@gmail.com>
2022-08-28 12:24:59 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Page from '../components/utility/Page'
 
 const Home: NextPage = () => {
   return (
-  <Page currentPage="Home" meta={{ desc: "UMANG.DEV, one stop to all things Umang Chauhan" }}>
+  <Page currentPage="Home" meta={{ desc: "UMANG.DEV, one stop to all things Umang Chauhan. Find out insights from the statistics tab or know more about me from the About Me page. Whatever you do stay away from the 404 Page." }}>
     <Hero />
   </Page>
   )
