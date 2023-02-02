17912458b18abb724b1ff20be3d5e1e0bbd30898
Umang Chauhan <umang350@gmail.com>
2023-02-02 20:52:30 +0900

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -18,10 +18,10 @@ function Page404() {
     }}>
       {num404 !== "0000" && (
         <div className="min-h-screen w-full flex items-center justify-center flex-col animate-fadeIn">
-          <h1 className="text-7xl text-white font-monospace font-bold opacity-100">{`{ error: ${num404} }`}</h1>
-          <p className="text-fun-gray text-xl mt-8 flex items-center">
+          <h1 className="text-4xl sm:text-7xl text-white font-monospace font-bold opacity-100">{`{ error: ${num404} }`}</h1>
+          <p className="text-fun-gray text-xl mt-8 flex flex-wrap justify-center items-center">
             Sorry, looks like that page is missing!&nbsp;&nbsp;
-            <Link href="/">
+            <Link href="./">
               <span className="w-full sm:w-auto flex-shrink border border-fun-pink-light text-base px-4 py-1 rounded-xl text-fun-pink-light bg-fun-pink-darkerer hover:bg-fun-pink hover:text-white transition-colors cursor-pointer">
                 Return Home
               </span>
